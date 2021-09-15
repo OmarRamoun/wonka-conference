f98dfeeb4f5c5efdb6f170ebbc30950c2267dc84
MrRamoun <omartarekramoun@gmail.com>
2021-09-16 01:44:30 +0200

    Add a new class for the 13px size

--- js/speakers.js
+++ js/speakers.js
@@ -47,7 +47,7 @@ for (let i = 0; i < speakers.length; i += 1) {
 
   // Adding Image
   const speakerImage = document.createElement('img');
-  speakerImage.classList.add('align-self-stretch');
+  speakerImage.classList.add('align-self-start');
   speakerImage.src = speakers[i].image;
   speakerImage.alt = speakers[i].name;
   speakerCard.appendChild(speakerImage);
@@ -58,19 +58,19 @@ for (let i = 0; i < speakers.length; i += 1) {
 
   // Adding speaker name
   const speakerName = document.createElement('h3');
-  speakerName.classList.add('fw-bold', 'text-capitalize');
+  speakerName.classList.add('fw-bold', 'fs-5', 'text-capitalize');
   speakerName.innerHTML = speakers[i].name;
   speakerInfo.appendChild(speakerName);
 
   // Adding speaker subtitle
   const speakerSubtitle = document.createElement('h4');
-  speakerSubtitle.classList.add('accent', 'fs-6', 'fst-italic', 'text-left');
+  speakerSubtitle.classList.add('accent', 'fs-20', 'fst-italic', 'text-left');
   speakerSubtitle.innerHTML = speakers[i].subtitle;
   speakerInfo.appendChild(speakerSubtitle);
 
   // Adding speaker description
   const speakerDesc = document.createElement('p');
-  speakerDesc.classList.add('fs-6', 'mb-0');
+  speakerDesc.classList.add('fs-20', 'mb-0');
   speakerDesc.innerHTML = speakers[i].desc;
   speakerInfo.appendChild(speakerDesc);
 
